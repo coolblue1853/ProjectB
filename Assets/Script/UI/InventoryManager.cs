6ea18491bf75116a93da8cfe98199c1f87e2da81
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2024-07-17 16:00:42 +0900

    [추가] 공격 효과음 추가 완료

--- Assets/Script/UI/InventoryManager.cs
+++ Assets/Script/UI/InventoryManager.cs
@@ -1437,7 +1437,7 @@ public class InventoryManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F2))
         {
           //  CreatItem("ScareSide");
-            CreatItem("SacreGreatSword"); //CreatItem("RatCheifDagger");
+            CreatItem("ScareClow"); CreatItem("ScareSide");
             //   CreatItem("ScareClow");
             //   CreatItem("ScareStaff");
             //   CreatItem("SacreGreatSword");
