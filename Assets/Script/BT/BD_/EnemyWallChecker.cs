cf705501b23f54d494dc16ec1e538ccbad01ad3b
coolblue1853 <54536107+coolblue1853@users.noreply.github.com>
2024-11-14 21:29:40 +0900

    (수정) BT 관련 최적화 완료

--- Assets/Script/BT/BD_/EnemyWallChecker.cs
+++ Assets/Script/BT/BD_/EnemyWallChecker.cs
@@ -1,67 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using BehaviorDesigner.Runtime.Tasks;
 using BehaviorDesigner.Runtime;
 using DG.Tweening;
+
 public class EnemyWallChecker : EnemyConditional
 {
-    BehaviorTree bt;
+    private BehaviorTree bt;
     public GameObject wallCheck;
-    RaycastHit2D hit2;
     public float wallRay = 0.2f;
-    public Vector2 startPos;
-    public Vector2 EndPos;
-     bool isEndWait = false;
+
     public override void OnStart()
     {
-        bt = this.transform.GetComponent<BehaviorTree>();
-        startPos = this.transform.position;
-        /* 이 부분이 있어서 kill 이 작동하지 않는 것이었음. 점프시에 돌아버리는 문제가 발생하면 여기 바꾸야함.
-       bt.isJumping = true;
-       bt.sequence.Kill();
-        bt.sequence = DOTween.Sequence()
-           .AppendInterval(2f)
-         .OnComplete(() => bt.isJumping = false);
-        */
-
-
+        bt = GetComponent<BehaviorTree>();
     }
-    public bool isGroundCheck = false;
 
     public override TaskStatus OnUpdate()
     {
-        chInRommSize = enemyObject.transform.localScale.x;
-        if (chInRommSize > 0)
-        {
-            hit2 = Physics2D.Raycast(wallCheck.transform.position, Vector2.right, wallRay, LayerMask.GetMask("Wall"));
-            Debug.DrawRay(wallCheck.transform.position, Vector2.right, Color.blue, wallRay);
-        }
-        else
-        {
-            hit2 = Physics2D.Raycast(wallCheck.transform.position, Vector2.left, wallRay, LayerMask.GetMask("Wall"));
-            Debug.DrawRay(wallCheck.transform.position, Vector2.left, Color.blue, wallRay);
-        }
+        // 현재 적의 방향에 따라 레이 방향을 설정합니다.
+        Vector2 rayDirection = enemyObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
 
-        if (hit2.collider == null) //right
-        {
-            return TaskStatus.Failure;
+        // 벽 검출 레이캐스트 실행
+        RaycastHit2D hit2 = Physics2D.Raycast(wallCheck.transform.position, rayDirection, wallRay, LayerMask.GetMask("Wall"));
 
-        }
-        else
-        {
+        // 디버그 레이 그리기
+     //   Debug.DrawRay(wallCheck.transform.position, rayDirection * wallRay, Color.blue);
 
-            /*
-            EndPos = this.transform.position;
-            if(Mathf.Abs(startPos.x)- Mathf.Abs(EndPos.x) < 0.1 || Mathf.Abs(startPos.y) - Mathf.Abs(EndPos.y) < 0.1)
-            {
-                Debug.Log("벽에 닿음2");
-                return TaskStatus.Failure;
-            }
-            */
-            return TaskStatus.Success;
-        }
+        // 레이캐스트 결과에 따라 TaskStatus 반환
+        return hit2.collider != null ? TaskStatus.Success : TaskStatus.Failure;
     }
-
-
 }
